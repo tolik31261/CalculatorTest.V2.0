cc41022c5be657d2b6fcecddaa75f4cc5c88ab4b
17152 <nazarovtolya2@gmail.com>
2023-06-18 22:16:59 +0500

    Начало ДЗ

---
+++ src/main/java/com/example/calculatortest/CalculatorTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calculatortest/CalculatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
